23224f31c7913c07620b56a557ef35f70a69ffed
gregmuldunna <bobsaludo@gmail.com>
2016-02-15 15:21:23 -0800

    Can add special char, a num, and choose num words

--- src/GeneratorSettings.php
+++ src/GeneratorSettings.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Configuration of settings to be given to the password generator.
+ * Better than making a function and passing in all these settings as a parameter.
+ */
 class GeneratorSettings{
 
 	// Default settings
@@ -7,6 +11,7 @@ class GeneratorSettings{
 	public $numWords = 4;
 	public $hasNum = false;
 	public $hasSpclChar = false;
+	public $delimiter = "-";
 
 	// Possible Special Characters
 	// According to University of Wisconsin-Madison https://kb.wisc.edu/page.php?id=4073
--- src/PassGenerator.php
+++ src/PassGenerator.php
@@ -3,28 +3,61 @@
 require "GeneratorSettings.php";
 
 /**
- * 
+ * Will generate a passphrase based on the settings given by a GenneratorSettings object.
+ * This won't be initialized without a GeneratorSettings object being passed in
  */
 class PassGenerator{
 
 	private $settings;
 
+	/**
+	 * PassGenerator will only initialize with a GeneratorSettings object passed in
+	 * 
+	 * @param GeneratorSettings $settings - defines the configurations of the 
+	 * passphrase such as number of words, whether to have numbers, and 
+	 * whether to have special characters, etc
+	 */
 	public function __construct(GeneratorSettings $settings){
 		$this->settings = $settings;
 	}
 
+
+	/**
+	 * Takes the settings passed in from initializing the GeneratorSettings object, 
+	 * and uses those settings to return a passphrase from randomly picked out words
+	 * from the $settings's wordlist
+	 * 
+	 * @return String - passphrase based on the configurations of $settings. 
+	 */
 	public function generate(){
+		$settings = $this->settings;
 		$delimiter = "-";
 		$chosenWords = [];
-		$wordlist = $this->settings->wordlist;
+		$wordlist = $settings->wordlist;
+		$numWords = $settings->numWords;
 
 		// Randomly pick a new word to go in sequence
-		for($i = 0; $i < 4; $i++){
+		for($i = 0; $i < $numWords; $i++){
 			$random_i = rand(0, count($wordlist)-1);
 			$chosenWords[] = $wordlist[$random_i];
 		}
 
-		return implode("-", $chosenWords);		
+		$passphrase = implode("-", $chosenWords);	
+
+
+		// add num
+		if($settings->hasNum){
+			$passphrase .= rand(0,9);
+		}
+
+		// add special character
+		if($settings->hasSpclChar){
+			$spclChar = GeneratorSettings::$possibleSpclChar;
+			$passphrase .= $spclChar[rand(0, count($spclChar)-1)];
+		}
+
+
+		return $passphrase;
 	}
 }
 
