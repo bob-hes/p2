3c45cba2da200cd38ddeeb3dd0f368918946c8e9
gregmuldunna <bobsaludo@gmail.com>
2016-02-15 11:50:51 -0800

    First project, and created structures for unit testing

---
+++ src/GeneratorSettings.php
@@ -0,0 +1,19 @@
+<?php
+
+class GeneratorSettings{
+
+	// Default settings
+	public $wordlist = ["a","bb","ccc","dddd"];
+	public $numWords = 4;
+	public $hasNum = false;
+	public $hasSpclChar = false;
+
+	// Possible Special Characters
+	// According to University of Wisconsin-Madison https://kb.wisc.edu/page.php?id=4073
+	public static $possibleSpclChar = ["!", "\"", "#","$","%","&","'","(",")","*","+",",","-",".","/",":",";", "<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+}
+
+
+
+
+?>
---
+++ src/PassGenerator.php
@@ -0,0 +1,28 @@
+<?php
+
+require "GeneratorSettings.php";
+
+class PassGenerator{
+
+	private $settings;
+
+	public function __construct(GeneratorSettings $settings){
+		$this->settings = $settings;
+	}
+
+	public function generate(){
+		$delimiter = "-";
+		$chosenWords = [];
+		$wordlist = $this->settings->wordlist;
+
+		// Randomly pick a new word to go in sequence
+		for($i = 0; $i < 4; $i++){
+			$random_i = rand(0, count($wordlist)-1);
+			$chosenWords[] = $wordlist[$random_i];
+		}
+
+		return implode("-", $chosenWords);		
+	}
+}
+
+?>
